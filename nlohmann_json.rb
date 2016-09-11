eeb7940e5b0b7865328faf1aa14a21443cefd8fc
Niels <niels.lohmann@gmail.com>
2016-09-11 23:41:08 +0200

    v2.0.4

--- nlohmann_json.rb
+++ nlohmann_json.rb
@@ -2,7 +2,7 @@ class NlohmannJson < Formula
   desc "JSON for Modern C++"
   homepage "https://github.com/nlohmann/json"
   url "https://github.com/nlohmann/json/archive/v2.0.3.tar.gz"
-  sha256 "4aad0a70112c2d461a0d3e553b4050f4cd9a616d757709e59c97cf13a763340f"
+  sha256 "d1951bf0aa7ba9c55fc660fcb7b22bef9135f5466a086aa8d44d723872b20409"
   head "https://github.com/nlohmann/json.git", :branch => "develop"
 
   def install
